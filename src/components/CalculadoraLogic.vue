835f30588a095d98e8051914280cd681b5456ad9
Eduardo <eduardoantune12@gmail.com>
2025-05-26 22:30:28 -0300

    Tive uma idéia inicial muito complicada, acabei decidindo diminuir a quantidade de componentes do projeto para facilitar minha jornada.

--- src/components/CalculadoraLogic.vue
+++ src/components/CalculadoraLogic.vue
@@ -1,32 +1,32 @@
 <script>
 export default {
-  data() {
-    return {
-      numero1: 0,
-      numero2: 0,
-      operacao: "+",
-      resultado: 0
+    data() {
+        return {
+            numero1: 0,
+            numero2: 0,
+            operacao: "+",
+            resultado: 0
+        }
+    },
+    methods: {
+        calcular() {
+            const num1 = parseFloat(this.numero1)
+            const num2 = parseFloat(this.numero2)
+            switch (this.operacao) {
+                case "+":
+                    this.resultado = num1 + num2
+                    break
+                case "-":
+                    this.resultado = num1 - num2
+                    break
+                case "*":
+                    this.resultado = num1 * num2
+                    break
+                case "/":
+                    this.resultado = num1 / num2
+                    break
+            }
+        }
     }
-  },
-  methods: {
-    calcular() {
-      const num1 = parseFloat(this.numero1)
-      const num2 = parseFloat(this.numero2)
-      switch (this.operacao) {
-        case "+":
-          this.resultado = num1 + num2
-          break
-        case "-":
-          this.resultado = num1 - num2
-          break
-        case "*":
-          this.resultado = num1 * num2
-          break
-        case "/":
-          this.resultado = num1 / num2
-          break
-      }
-    }
-  }
 }
 </script>
--- src/components/CalculadoraTemplate.vue
+++ src/components/CalculadoraTemplate.vue
@@ -1,37 +1,37 @@
 <template>
-  <div class="container mt-5">
-    <div class="row">
-      <div class="col-md-6 offset-md-3">
-        <h1 class="text-center">Calculadora</h1>
-        <form>
-          <div class="form-group">
-            <label for="numero1">Número 1:</label>
-            <input type="number" class="form-control" id="numero1" v-model="numero1" @input="calcular" />
-          </div>
-          <div class="form-group">
-            <label for="numero2">Número 2:</label>
-            <input type="number" class="form-control" id="numero2" v-model="numero2" @input="calcular" />
-          </div>
-          <div class="form-group">
-            <label for="operacao">Operação:</label>
-            <select class="form-control" id="operacao" v-model="operacao" @change="calcular">
-              <option value="+">Somar</option>
-              <option value="-">Subtrair</option>
-              <option value="*">Multiplicar</option>
-              <option value="/">Dividir</option>
-            </select>
-          </div>
-          <p class="text-center">Resultado: {{ resultado }}</p>
-        </form>
-      </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <h1 class="text-center">Calculadora</h1>
+                <form>
+                    <div class="form-group">
+                        <label for="numero1">Número 1:</label>
+                        <input type="number" class="form-control" id="numero1" v-model="numero1" @input="calcular" />
+                    </div>
+                    <div class="form-group">
+                        <label for="numero2">Número 2:</label>
+                        <input type="number" class="form-control" id="numero2" v-model="numero2" @input="calcular" />
+                    </div>
+                    <div class="form-group">
+                        <label for="operacao">Operação:</label>
+                        <select class="form-control" id="operacao" v-model="operacao" @change="calcular">
+                            <option value="+">Somar</option>
+                            <option value="-">Subtrair</option>
+                            <option value="*">Multiplicar</option>
+                            <option value="/">Dividir</option>
+                        </select>
+                    </div>
+                    <p class="text-center">Resultado: {{ resultado }}</p>
+                </form>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import CalculadoraLogic from './CalculadoraLogic.vue'
 
 export default {
-  mixins: [CalculadoraLogic]
+    mixins: [CalculadoraLogic]
 }
 </script>
